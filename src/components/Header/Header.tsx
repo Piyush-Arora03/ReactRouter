6752554019d51e34be9d707f1e2013ae85fc8278
Piyush-Arora03 <piyush.2023ug1113@iiitranchi.ac.in>
2026-01-12 00:29:44 +0530

    Added Footer and some code cleanup

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
 
 function Header() {
   return (
-    <header className='sticky shadow top-0'>
-        <nav className='bg-white border-gray-200 px-4 py-2.5'>
+    <header className='sticky shadow top-0 backdrop-blur z-50'>
+        <nav className='border-gray-200 px-4 py-2.5'>
             <div className='justify-between flex flex-wrap items-center mx-auto max-w-7xl'>
                 <Link to="/" className='flex items-center'>
                     <img 
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,28 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { BrowserRouter, createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './components/Home/Home.tsx'
+import Contact from './components/Contact/Contact.jsx'
+import About from './components/About/About.tsx'
 const routes=createBrowserRouter([
   {
     path:'/',
-    element:<App/>
+    element:<App/>,
+    children:[
+      {
+        path:"",
+        element:<Home/>
+      },
+      {
+        path:"contact",
+        element:<Contact/>
+      },
+      {
+        path:"about",
+        element:<About/>
+      }
+    ]
   }
 ])
 
